d46fed24b05fceb3454fa36058c60b388ecd11fd
jingdongwei <youciii@163.com>
2018-08-17 13:55:32 +0800

    1. 解决自桌面返回后状态栏变回不透明状态问题: 小米手机点击App时新建界面的原因;

--- android/app/src/main/java/com/rndemo/MainActivity.java
+++ android/app/src/main/java/com/rndemo/MainActivity.java
@@ -1,11 +1,21 @@
 package com.rndemo;
 
+import android.os.Bundle;
 import android.content.Intent;
 import android.content.res.Configuration;
-
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 解决桌面返回后状态栏变回不透明状态问题: 小米手机点击App时新建界面的原因
+        if (!isTaskRoot() || ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0)) {
+            finish();
+            return;
+        }
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
